d1daa6d43fa0f67af5e3cc1572b33f7558da4db3
Timur Potapchuk <timkrasava97@gmai.com>
2022-09-04 16:35:53 +0300

    Creature array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[6] {"123", "945", "hello", "world", "new", "password"};
+string[] array2 = new string[array1.Length];
